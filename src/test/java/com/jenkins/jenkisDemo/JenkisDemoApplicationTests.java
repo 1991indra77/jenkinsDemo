f9845b67dce6d265c407e42793581dcdc8091774
Indranil Chowdhury <chowdhury.indranil007@gmail.com>
2022-10-11 01:53:18 -0700

    first commit

---
+++ src/test/java/com/jenkins/jenkisDemo/JenkisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkisDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
